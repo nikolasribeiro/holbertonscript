b1fb7e54b0d50042036eda96393096b07df504dd
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-01-25 17:02:45 -0300

    adding the variable location_language

--- INSTALLER.py
+++ INSTALLER.py
@@ -6,6 +6,8 @@ Author: Nicolas Ribeiro
 """
 import os
 
+LOCATION_LANGUAGE = "es_UY.UTF-8"
+
 # Installing main dependences
 print("...:::/// Installing all the neccessary dependences ///:::...")
 os.system("sudo apt install python3-pip -y")
@@ -16,7 +18,7 @@ os.system("sudo pip3 install yapf")
 os.system("sudo pip3 install black")
 os.system("sudo apt-get install clang-format-3.4")
 os.system("sudo apt-get install clang-format")
-os.system("sudo locale-gen es_UY.UTF-8")
+os.system("sudo locale-gen {}".format(LOCATION_LANGUAGE))
 os.system("sudo dpkg-reconfigure locales")
 os.system("cat script.py > up && sudo mv up /usr/local/bin && sudo chmod +x /usr/local/bin/up")
 
